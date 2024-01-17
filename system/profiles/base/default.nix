a545953c2dcb970f11fa79b98bbd50c2da7edb21
ooks-io <ooks@protonmail.com>
2024-01-17 23:03:48 +1300

    add: more system modules

--- system/profiles/base/default.nix
+++ system/profiles/base/default.nix
@@ -1,17 +1,28 @@
-{ lib, config, ... }:
+{ inputs, outputs, lib, config, pkgs, ... }:
 
 let
   cfg = config.systemProfile.base;
 in
 
 {
+
+  imports = [
+    ../modules
+    inputs.home-manager.nixosModules.home-manager
+  ];
+  
   config = lib.mkIf cfg.enable {
+    environment.systemPackages = [pkgs.git];
+    environment.enableAllTerminfo = true;
     systemModules = {
       security.enable = true;
       nixOptions.enable = true;
       pipewire.enable = true;
       networking.enable = true;
       locale.enable = true;
-    }
+    };
+
+    home-manager.extraSpecialArgs = { inherit inputs outputs; };
+    hardware.enableAllFirmware = true;
   }; 
 }
