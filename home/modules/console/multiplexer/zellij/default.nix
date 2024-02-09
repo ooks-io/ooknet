b4030c16b9f8e74b3130beb0911b49b7ca2ed22a
ooks-io <ooks@protonmail.com>
2024-02-09 22:04:39 +1300

    feat: zellij keybinds

--- home/modules/console/multiplexer/zellij/default.nix
+++ home/modules/console/multiplexer/zellij/default.nix
@@ -30,6 +30,21 @@ in
         };
       };
     };
+    xdg.configFile."zellij/config.kdl".text = /* kdl */ ''
+      keybinds {
+          shared_except "locked" {
+              bind "Alt 1" { GoToTab 1; }
+              bind "Alt 2" { GoToTab 2; }
+              bind "Alt 3" { GoToTab 3; }
+              bind "Alt 4" { GoToTab 4; }
+              bind "Alt 5" { GoToTab 5; }
+              bind "Alt 6" { GoToTab 6; }
+              bind "Alt 7" { GoToTab 7; }
+              bind "Alt 8" { GoToTab 8; }
+              bind "Alt 9" { GoToTab 9; }
+          }
+      }
+    '';
     xdg.configFile."zellij/layouts/base.kdl".text = /* kdl */ ''
       layout {
       default_tab_template {
