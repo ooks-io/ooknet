c8e6eba1a6fe0a54fe5e287e735312bf9e54b629
ooks-io <ooks@protonmail.com>
2024-02-09 14:42:10 +1300

    feat: setup colors for cava with home-manager options

--- home/modules/desktop/media/music/tui/default.nix
+++ home/modules/desktop/media/music/tui/default.nix
@@ -1,6 +1,7 @@
 { pkgs, config, lib, ... }:
 
 let
+  inherit (config.colorscheme) colors;
   cfg = config.homeModules.desktop.media.music.tui;
 in
 
@@ -11,8 +12,22 @@ in
       spotify-player
       ytui-music
       alsa-utils
-      cava
     ];
+    programs.cava = {
+      enable = true;
+      settings = {
+        general.framerate = 60;
+        color = {
+          gradient = 1;
+          gradient_count = 5;
+          gradient_color_1 = "'#${colors.base0A}'";
+          gradient_color_2 = "'#${colors.base0B}'";
+          gradient_color_3 = "'#${colors.base0C}'";
+          gradient_color_4 = "'#${colors.base0D}'";
+          gradient_color_5 = "'#${colors.base0E}'";
+        };
+      };
+    };
   };
   
 }
