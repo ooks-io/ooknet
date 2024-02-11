4c3c29c396c2fe321c65b2c7111ecf3a702c13f4
ooks-io <ooks@protonmail.com>
2024-02-11 21:14:28 +1300

    feat: add desktop (ooksdesk) home configuration file

---
+++ home/user/ooks/ooksdesk/default.nix
@@ -0,0 +1,30 @@
+{ inputs, outputs, config, ... }:
+
+{
+  imports = [
+    ../../../profile
+  ];
+
+  activeProfiles = ["base" "hyprland"];
+
+  homeModules.console.editor.nvim = {
+    enable = true;
+    plugins = {
+      lualine = true;
+      telescope = true;
+      indentBlankline = true;
+    };
+  };
+  
+  monitors = [{
+    name = "DP-1";
+    width = 2560;
+    height = 1440;
+    refreshRate = 155;
+    workspace = "1";
+    primary = true;
+  }];
+
+  colorscheme = inputs.nix-colors.colorSchemes.gruvbox-material-dark-soft;
+}
+
