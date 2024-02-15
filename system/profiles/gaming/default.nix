cbb415fd66016dfc6975e652cc6551bfc2e5a28b
ooks-io <ooks@protonmail.com>
2024-02-15 20:32:23 +1300

    feat: steam gamescope packages

--- system/profiles/gaming/default.nix
+++ system/profiles/gaming/default.nix
@@ -2,6 +2,23 @@
 
 let
   cfg = config.systemProfile.gaming;
+  steamFix = pkgs.steam.override {
+    extraPkgs = pkgs: with pkgs; [
+      xorg.libXcursor
+      xorg.libXi
+      xorg.libXinerama
+      xorg.libXScrnSaver
+      libpng
+      libpulseaudio
+      libvorbis
+      stdenv.cc.cc.lib
+      libkrb5
+      keyutils
+      mangohud
+      winetricks
+      protontricks
+    ];
+  };
 in
 
 {
@@ -12,7 +29,10 @@ in
   config = lib.mkIf cfg.enable {
     hardware.opengl.extraPackages = [ pkgs.gamescope ];
     programs = {
-      steam.enable = true;
+      steam = {
+        enable = true;
+        package = steamFix;
+      };
       gamescope = {
         enable = true;
         capSysNice = true;
