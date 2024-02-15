f569b473476f63f9a19264d0f4a5b7a540de70c0
ooks-io <ooks@protonmail.com>
2024-02-16 12:15:03 +1300

    refactor: jellyfin module > all in one mediaServer Module

---
+++ system/modules/services/mediaServer/default.nix
@@ -0,0 +1,49 @@
+{ config, lib, pkgs, ... }:
+
+let
+  cfg = config.systemModules.services.mediaServer;
+in
+
+{
+  config = lib.mkIf cfg.enable {
+
+    users.groups.media = { };
+
+    environment.systemPackages = with pkgs; [
+      jellyfin
+      jellyfin-web
+      jellyfin-ffmpeg 
+    ];
+
+    services = {
+      jellyfin = {
+        group = "media";
+        enable = true;
+        openFirewall = true;
+      };
+      deluge = {
+        group = "media";
+        enable = true;
+        web.enable = true;
+      };
+      radarr = {
+        group = "media";
+        enable = true;
+      };
+      sonarr = {
+        group = "media";
+        enable = true;
+      };
+      prowlarr.enable = true;
+    };
+
+    systemd.tmpfiles.rules = [
+      "d /jellyfin 0770 - media - -"
+    ];
+
+    fileSystems."/jellyfin" = {
+      device = "/dev/disk/by-label/jellyfin";
+      fsType = "btrfs";
+    };
+  };
+}
