c98df95f5e068be8f83bd3065366881502e7ee1a
ooks-io <ooks@protonmail.com>
2024-03-22 21:58:32 +1300

    feat(rofi): simple rofi theme

--- home/modules/desktop/wayland/launcher/rofi/default.nix
+++ home/modules/desktop/wayland/launcher/rofi/default.nix
@@ -10,7 +10,60 @@ in
       enable = true;
       font = "${config.fontProfiles.monospace.family}";
       package = pkgs.rofi-wayland;
+      terminal = "${config.home.sessionVariables.TERMINAL}";
+      theme = let
+        inherit (config.colorscheme ) colors;
+        inherit (config.lib.formats.rasi) mkLiteral;
+      in {
+        "*" = {
+          background = mkLiteral "#${colors.base00}";
+          foreground = mkLiteral "#${colors.base05}";
+          selected = mkLiteral "#${colors.base0B}";
+
+          background-color = mkLiteral "@background";
+          border-color = mkLiteral "@foreground";
+          text-color = mkLiteral "@foreground";
+          font = mkLiteral "'${config.fontProfiles.monospace.family} 14'";
+        };
+
+        "window" = {
+          width = mkLiteral "15%";
+          border = mkLiteral "2";
+          padding = mkLiteral "10";
+          children = mkLiteral "[inputbar,listview]";
+        };
+
+        "inputbar" = {
+          cursor = mkLiteral "pointer";
+          border = mkLiteral "2";
+          children = mkLiteral "[textbox-prompt-colon,entry]";
+        };
+
+        "entry" = {
+          cursor = mkLiteral "false";
+        };
+
+        "textbox-prompt-colon" = {
+          text-color = mkLiteral "@selected";
+          expand = mkLiteral "false";
+          margin = mkLiteral "0 0.3em 0em 0em";
+          str = mkLiteral "'  '";
+        };
+
+        "listview" = {
+          scrollbar = mkLiteral "true";
+          fixed-height = mkLiteral "false";
+          dynamic = mkLiteral "true";
+        };
+
+        "element-text" = {
+          horizontal-align = mkLiteral "0.50";
+        };
+
+        "element-text selected" = {
+          text-color = mkLiteral "@selected";
+        };
+      };
     };
   };
-  
 }
