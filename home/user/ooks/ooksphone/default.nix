eb754fb1638c7a1bfe74050f27fe862d4c3c3f4e
ooks-io <ooks@protonmail.com>
2024-04-20 22:34:07 +1200

    feat(ooksphone): add substituters/trusted public keys

--- home/user/ooks/ooksphone/default.nix
+++ home/user/ooks/ooksphone/default.nix
@@ -9,7 +9,12 @@
   theme.phone.enable = true;
 
   homeModules = {
-    console.editor.helix.enable = true;
+    console = {
+      editor.helix.enable = true;
+      shell.fish.enable = true;
+      prompt.starship.enable = true;
+      multiplexer.zellij.enable = true;
+    };
   };
 
   home.stateVersion = "23.11";
--- system/hosts/ooksphone/default.nix
+++ system/hosts/ooksphone/default.nix
@@ -15,10 +15,22 @@
 
   system.stateVersion = "23.11";
 
-  nix.extraOptions = ''
-    experimental-features = nix-command flakes
-  '';
-
+  nix = {
+    extraOptions = ''
+      experimental-features = nix-command flakes
+    '';
+    substituters = [
+      "https://cache.nixos.org?priority=10"
+      "https://helix.cachix.org"
+      "https://nix-community.cachix.org"
+    ];
+    trustedPublicKeys = [
+      "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+      "helix.cachix.org-1:ejp9KQpR1FBI2onstMQ34yogDm4OgU2ru6lIwPvuCVs="
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+    ];
+  };
+  
   time.timeZone = "Pacific/Auckland";
 
   user = {
