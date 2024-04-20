c602463326d7b3d571c9a4e989145188e11f6e74
ooks-io <ooks@protonmail.com>
2024-04-20 22:53:43 +1200

    fix(ooksphone): need to pass the zjstatus overlay

--- home/user/ooks/ooksphone/default.nix
+++ home/user/ooks/ooksphone/default.nix
@@ -13,9 +13,8 @@
       editor.helix.enable = true;
       shell.fish.enable = true;
       prompt.starship.enable = true;
-      multiplexer.zellij.enable = true;
+      # FIXME: multiplexer.zellij.enable = true;
     };
-    config.nix.enable = true;
   };
 
   home.stateVersion = "23.11";
