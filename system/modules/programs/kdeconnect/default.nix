c69b0d57e33ff381605898218af3dc98ffcb8783
ooks-io <ooks@protonmail.com>
2024-04-30 21:01:45 +1200

    refactor(systemModules:kdeconnect): change to always enable

--- system/modules/programs/kdeconnect/default.nix
+++ system/modules/programs/kdeconnect/default.nix
@@ -1,16 +1,7 @@
-{ lib, config, ... }:
-
-let
-  inherit (lib) mkEnableOption mkIf;
-  cfg = config.systemModules.services.kdeconnect;
-in
+{ ... }:
 
 {
-  options.systemModules.services.kdeconnect.enable = mkEnableOption "Enable kdeconnect system module";
-
-  config = mkIf cfg.enable {
-    programs.kdeconnect = {
-      enable = true;
-    };
+  programs.kdeconnect = {
+    enable = true;
   };
 }
