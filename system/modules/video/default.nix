b821ac5226af97ee677df37deeb75995b6e90e8f
ooks-io <ooks@protonmail.com>
2024-05-16 22:03:07 +1200

    refactor(systemModules:video): change mkIf condition to != phone

--- system/modules/video/default.nix
+++ system/modules/video/default.nix
@@ -5,16 +5,25 @@ let
   inherit (builtins) elem;
   isx86Linux = pkgs: with pkgs.stdenv; hostPlatform.isLinux && hostPlatform.isx86;
   host = config.systemModules.host;
-  validFunction = ["workstation" "gaming" "media-server"];
 in
 
 {
-  config = mkIf (elem host.function validFunction) {
+  config = mkIf (host.type != "phone") {
     hardware = {
       opengl = {
         enable = true;
         driSupport = true;
         driSupport32Bit = isx86Linux pkgs;
+      };
+    };
+    xdg.portal = {
+      enable = true;
+      extraPortals = [
+        pkgs.xdg-desktop-portal-gtk
+      ];
+      config = {
+        common.default = ["gtk"];
+        hyprland.default = ["gtk" "hyprland"];
       };
     };
   };
