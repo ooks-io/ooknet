cba4323fb721b2fbc31db23d5c0beec6adf7476c
ooks-io <ooks@protonmail.com>
2024-05-24 19:22:48 +1200

    feat(homeModules:hyprland): change to hyprland flake package

--- inputs/home/modules/desktop/wayland/windowManager/hyprland/default.nix
+++ inputs/home/modules/desktop/wayland/windowManager/hyprland/default.nix
@@ -24,6 +24,7 @@ in
     
     wayland.windowManager.hyprland = {
       enable = true;
+      package = inputs.hyprland.packages.${pkgs.system}.hyprland;
       xwayland.enable = true;
       systemd = {
         enable = true;
--- inputs/home/modules/desktop/wayland/windowManager/hyprland/settings/rules.nix
+++ inputs/home/modules/desktop/wayland/windowManager/hyprland/settings/rules.nix
@@ -6,12 +6,12 @@ in
   config = {
     wayland.windowManager.hyprland.settings = lib.mkIf cfg.enable {
       windowrulev2 = [
-        "float,move 191 15,size 924 396,class:(1Password)"
+        "float,move 191 15,size 924 396,class:^(1Password)$"
 
         "float, title:^(Picture-in-Picture)$"
         "pin, title:^(Picture-in-Picture)$"
 
-        "workspace 4, title:Vesktop"
+        "workspace 4, title:^(Vesktop)$"
 
         # Floating BTOP
         "float,title:^(BTOP)$"
