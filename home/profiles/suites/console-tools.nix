6cdfe70495ea49bb7f9080a6bc8d64f505653270
ooks-io <ooks@protonmail.com>
2024-06-12 22:26:18 +1200

    feat(home:profiles): add console-tools suite

---
+++ home/profiles/suites/console-tools.nix
@@ -0,0 +1,25 @@
+{ osConfig, lib, ... }:
+
+let
+  inherit (lib) mkIf;
+  inherit (builtins) elem;
+  profiles = osConfig.ooknet.host.profiles;
+in
+
+{
+  config = mkIf (elem "console-tools" profiles) {
+    ooknet.tools = {
+      btop.enable = true;
+      eza.enable = true;
+      bat.enable = true;
+      direnv.enable = true;
+      fzf.enable = true;
+      git.enable = true;
+      ssh.enable = true;
+      nixIndex.enable = true;
+      starship.enable = true;
+      utils.enable = true;
+      ffmpeg.enable = true;
+    };
+  };
+}
