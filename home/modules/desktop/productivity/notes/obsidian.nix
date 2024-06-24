a049492c98a1edaa5f7d3cb21a11860d50458760
ooks-io <ooks@protonmail.com>
2024-06-24 18:15:09 +1200

    feat(home:obsidian): add activation script for obsidian vault repo

--- home/modules/desktop/productivity/notes/obsidian.nix
+++ home/modules/desktop/productivity/notes/obsidian.nix
@@ -1,15 +1,24 @@
 { lib, config, pkgs, ... }:
 
 let
-  inherit (lib) mkIf mkMerge;
+  inherit (lib) mkIf mkMerge hm;
   cfg = config.ooknet.productivity.notes.obsidian;
   notes = config.ooknet.desktop.notes;
+  # admin = osConfig.ooknet.host.admin;
+  # TODO: use admin.githubUsername
+  notesRepo = "git@github.com:ooks-io/notes.git";
+  notesPath = "${config.xdg.userDirs.documents}/notes";
 in
 
 {
   config = mkMerge [ 
     (mkIf (cfg.enable || notes == "obsidian") {
       home.packages = [ pkgs.obsidian ];
+      home.activation.cloneObsidianVault = hm.dag.entryAfter ["installPackages"] /* shell */ ''
+        if ! [ -d "${notesPath}" ]; then
+          $DRY_RUN_CMD git clone ${notesRepo} ${notesPath}
+        fi
+      '';
     })
 
     (mkIf (notes == "obsidian") {
