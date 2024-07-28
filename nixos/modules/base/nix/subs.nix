4a40a7bf033460414b3d6ec68c00452ba172f89f
ooks-io <ooks@protonmail.com>
2024-07-28 15:37:20 +1200

    feat(nixos:subs): add nvf cachix

--- nixos/modules/base/nix/subs.nix
+++ nixos/modules/base/nix/subs.nix
@@ -14,6 +14,7 @@ in
         "https://hyprland.cachix.org"
         "https://nix-community.cachix.org"
         "https://nixpkgs-wayland.cachix.org"
+        "https://neovim-flake.cachix.org"
       ];
 
       trusted-public-keys = [
@@ -22,6 +23,7 @@ in
         "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
         "nixpkgs-wayland.cachix.org-1:3lwxaILxMRkVhehr5StQprHdEo4IrE8sRho9R9HOLYA="
+        "neovim-flake.cachix.org-1:iyQ6lHFhnB5UkVpxhQqLJbneWBTzM8LBYOFPLNH4qZw="
       ];
     };
   };
