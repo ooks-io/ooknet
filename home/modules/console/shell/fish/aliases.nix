0442fdc4eeb5c0a9308b1ee07b0d516b8117c178
ooks-io <ooks@protonmail.com>
2024-07-28 15:29:54 +1200

    feat(home:fish): add gitroot alias

--- home/modules/console/shell/fish/aliases.nix
+++ home/modules/console/shell/fish/aliases.nix
@@ -16,6 +16,7 @@ in
         ls = "${getExe eza} -a --icons --group-directories-first";
         lst = "${getExe eza} -T -L 5 --icons --group-directories-first";
         du = "${getExe dust}";
+        gitroot = "cd (git rev-parse --show-toplevel)";
       };
       shellAbbrs = {
         f = "cd $FLAKE";
