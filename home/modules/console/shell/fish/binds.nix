17e2ecccdddcb04d7184720fe52f19e2d7e64a61
ooks-io <ooks@protonmail.com>
2024-08-02 21:13:05 +1200

    feat(home:fish): add nvim telescope binding

--- home/modules/console/shell/fish/binds.nix
+++ home/modules/console/shell/fish/binds.nix
@@ -10,11 +10,13 @@
   cfg = config.ooknet.shell.fish;
 in {
   config = mkIf (cfg.enable || admin.shell == "fish") {
-    programs.fish.functions = {
-      fish_user_key_bindings = ''
-        bind --preset -M insert \cf $EDITOR $FLAKE
-        bind --preset -M insert \ec fzf_cd_widget
-      '';
+    programs.fish = {
+      functions = {
+        fish_user_key_bindings = ''
+          bind --preset -M insert \cf nvim '+Telescope find_files' $FLAKE
+          bind --preset -M insert \ec fzf_cd_widget
+        '';
+      };
     };
   };
 }
