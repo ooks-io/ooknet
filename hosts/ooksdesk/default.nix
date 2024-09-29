7f8289082b29deb235b1fafba8e0af92e7529611
ooks-io <ooks@protonmail.com>
2024-09-29 20:29:53 +1300

    nixos(ooksdesk): swap to new monitor

--- hosts/ooksdesk/default.nix
+++ hosts/ooksdesk/default.nix
@@ -33,9 +33,9 @@
         {
           name = "DP-1";
           primary = true;
-          width = 2560;
-          height = 1440;
-          refreshRate = 155;
+          width = 1920;
+          height = 1080;
+          refreshRate = 60;
           workspace = "1";
         }
       ];
