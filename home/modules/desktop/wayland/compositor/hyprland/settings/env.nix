4b1a06c65737660b2244aab1cba175daf3a6a0b1
ooks-io <ooks@protonmail.com>
2024-10-09 14:08:27 +1300

    home(hyprland): fix incorrect xdg env vars

--- home/modules/desktop/wayland/compositor/hyprland/settings/env.nix
+++ home/modules/desktop/wayland/compositor/hyprland/settings/env.nix
@@ -8,8 +8,8 @@
 in {
   config = mkIf (wayland.compositor == "hyprland") {
     wayland.windowManager.hyprland.settings.env = [
-      "XDG_SESSION_DESKTOP,hyprland"
-      "XDG_CURRENT_DESKTOP,hyprland"
+      "XDG_SESSION_DESKTOP,Hyprland"
+      "XDG_CURRENT_DESKTOP,Hyprland"
     ];
   };
 }
