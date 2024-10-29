5e72233400ed8c8b586e3e991687e2bebfc9578d
ooks-io <ooks@protonmail.com>
2024-10-29 16:42:39 +1100

    firewall: add ssh ports

--- modules/nixos/base/security/firewall.nix
+++ modules/nixos/base/security/firewall.nix
@@ -2,6 +2,12 @@
   networking.firewall = {
     enable = true;
 
+    allowedTCPPorts = [
+      22 # SSH
+      80
+      443
+    ];
+
     # dont respond to icmpv4 pings.
     allowPing = false;
   };
