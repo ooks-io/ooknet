5688d97e0c83d4491c8f7992f00423690b8d943e
ooks-io <ooks@protonmail.com>
2024-11-09 22:49:57 +1100

    home: move foot color configuration -> hozen

--- modules/home/workstation/terminal/foot.nix
+++ modules/home/workstation/terminal/foot.nix
@@ -1,10 +1,12 @@
 {
   osConfig,
   lib,
+  hozen,
   ...
 }: let
   inherit (osConfig.ooknet.appearance) colorscheme fonts;
   inherit (colorscheme) palette;
+  inherit (hozen) color;
   inherit (lib) mkMerge mkIf;
   inherit (osConfig.ooknet.workstation) default;
   cfg = osConfig.ooknet.workstation.programs.foot;
@@ -41,30 +43,30 @@ in {
 
           colors = {
             alpha = 1.0;
-            foreground = "${palette.base05}";
-            background = "${palette.base00}";
-            regular0 = "${palette.base00}"; # black
-            regular1 = "${palette.red}"; # red
-            regular2 = "${palette.green}"; # green
-            regular3 = "${palette.yellow}"; # yellow
-            regular4 = "${palette.blue}"; # blue
-            regular5 = "${palette.purple}"; # magenta
-            regular6 = "${palette.cyan}"; # cyan
-            regular7 = "${palette.base05}"; # white
-            bright0 = "${palette.base03}"; # bright black
-            bright1 = "${palette.bright-red}"; # bright red
-            bright2 = "${palette.bright-green}"; # bright green
-            bright3 = "${palette.bright-yellow}"; # bright yellow
-            bright4 = "${palette.bright-blue}"; # bright blue
-            bright5 = "${palette.bright-purple}"; # bright magenta
-            bright6 = "${palette.bright-cyan}"; # bright cyan
-            bright7 = "${palette.base07}"; # bright white
-            "16" = "${palette.base09}";
-            "17" = "${palette.base0F}";
-            "18" = "${palette.base01}";
-            "19" = "${palette.base02}";
-            "20" = "${palette.base04}";
-            "21" = "${palette.base06}";
+            foreground = "${color.base05}";
+            background = "${color.base00}";
+            regular0 = "${color.base00}"; # black
+            regular1 = "${color.base08}"; # red
+            regular2 = "${color.base0B}"; # green
+            regular3 = "${color.base0A}"; # yellow
+            regular4 = "${color.base0D}"; # blue
+            regular5 = "${color.base0E}"; # magenta
+            regular6 = "${color.base0C}"; # cyan
+            regular7 = "${color.base05}"; # white
+            bright0 = "${color.base03}"; # bright black
+            bright1 = "${color.base08}"; # bright red
+            bright2 = "${color.base0B}"; # bright green
+            bright3 = "${color.base0A}"; # bright yellow
+            bright4 = "${color.base0D}"; # bright blue
+            bright5 = "${color.base0E}"; # bright magenta
+            bright6 = "${color.base0C}"; # bright cyan
+            bright7 = "${color.base07}"; # bright white
+            "16" = "${color.base09}";
+            "17" = "${color.base0F}";
+            "18" = "${color.base01}";
+            "19" = "${color.base02}";
+            "20" = "${color.base04}";
+            "21" = "${color.base06}";
           };
         };
       };
