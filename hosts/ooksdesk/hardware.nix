f9958ab4f7d281370f9506c427b5b3874ff5cd80
ooks-io <ooks@protonmail.com>
2025-01-05 23:48:27 +1100

    ooksdesk: swap to new monitor

--- hosts/ooksdesk/hardware.nix
+++ hosts/ooksdesk/hardware.nix
@@ -6,12 +6,22 @@
     features = ["printing" "ssd" "audio" "video"];
     monitors = [
       {
-        name = "DP-3";
+        name = "DP-1";
         primary = true;
+        width = 2560;
+        height = 1440;
+        refreshRate = 144;
+        workspace = "1";
+        x = 1920;
+        y = 100;
+      }
+      {
+        name = "DP-2";
         width = 1920;
         height = 1080;
         refreshRate = 180;
-        workspace = "1";
+        x = 840;
+        transform = 1;
       }
     ];
   };
