f895fbaf3f53014d4de08487fe0286395b23c350
ooks-io <ooks@protonmail.com>
2025-02-01 23:42:50 +1100

    openssh: client hardening

--- modules/nixos/base/openssh.nix
+++ modules/nixos/base/openssh.nix
@@ -1,10 +1,34 @@
-{
+let
+  # restict key exchange, cipher, and MAC algorithms, as per <https://www.ssh-audit.com>
+  KexAlgorithms = [
+    "sntrup761x25519-sha512@openssh.com"
+    "curve25519-sha256"
+    "curve25519-sha256@libssh.org"
+    "diffie-hellman-group18-sha512"
+    "diffie-hellman-group-exchange-sha256"
+    "diffie-hellman-group16-sha512"
+  ];
+  Ciphers = [
+    "chacha20-poly1305@openssh.com"
+    "aes256-gcm@openssh.com"
+    "aes128-gcm@openssh.com"
+    "aes256-ctr"
+    "aes192-ctr"
+    "aes128-ctr"
+  ];
+  Macs = [
+    "hmac-sha2-512-etm@openssh.com"
+    "hmac-sha2-256-etm@openssh.com"
+    "umac-128-etm@openssh.com"
+  ];
+in {
   services = {
     openssh = {
       enable = true;
       startWhenNeeded = true;
       ports = [22];
       settings = {
+        inherit KexAlgorithms Ciphers Macs;
         UseDns = true;
         PubkeyAuthentication = "yes";
         PermitRootLogin = "no";
@@ -15,36 +39,28 @@
         IgnoreRhosts = "yes";
 
         # by default openssh uses port 22
-
-        # restict key exchange, cipher, and MAC algorithms, as per <https://www.ssh-audit.com>
-        KexAlgorithms = [
-          "sntrup761x25519-sha512@openssh.com"
-          "curve25519-sha256"
-          "curve25519-sha256@libssh.org"
-          "diffie-hellman-group18-sha512"
-          "diffie-hellman-group-exchange-sha256"
-          "diffie-hellman-group16-sha512"
-          "ecdh-sha2-nistp256"
-        ];
-        Ciphers = [
-          "chacha20-poly1305@openssh.com"
-          "aes128-ctr"
-          "aes192-ctr"
-          "aes256-ctr"
-          "aes256-gcm@openssh.com"
-          "aes128-gcm@openssh.com"
-        ];
-        Macs = [
-          "hmac-sha2-512-etm@openssh.com"
-          "hmac-sha2-256-etm@openssh.com"
-          "umac-128-etm@openssh.com"
-        ];
       };
     };
+    # client
     fail2ban.jails.sshd.settings = {
       enable = true;
       filter = "sshd";
       mode = "aggressive";
     };
+  };
+  programs.ssh = {
+    startAgent = true;
+    macs = Macs;
+    kexAlgorithms = KexAlgorithms;
+    ciphers = Ciphers;
+    hostKeyAlgorithms = [
+      "sk-ssh-ed25519-cert-v01@openssh.com"
+      "ssh-ed25519-cert-v01@openssh.com"
+      "rsa-sha2-512-cert-v01@openssh.com"
+      "sk-ssh-ed25519@openssh.com"
+      "ssh-ed25519"
+      "rsa-sha2-512"
+      "rsa-sha2-256"
+    ];
   };
 }
