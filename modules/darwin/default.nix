9acc234f9a80fbffdd8023d74efc8d34681064ad
ooks-io <ooks@protonmail.com>
2025-04-17 14:31:09 +1000

    darwin: import common home-manager modules

--- modules/darwin/default.nix
+++ modules/darwin/default.nix
@@ -1,1 +1,24 @@
-{}
+{
+  lib,
+  config,
+  self,
+  ...
+}: let
+  inherit (lib) optionalAttrs;
+  inherit (config.ooknet.host) admin guest;
+in {
+  imports = [
+    ./system.nix
+  ];
+  home-manager.users =
+    (optionalAttrs admin.homeManager {
+      "${admin.name}" = {
+        imports = ["${self}/modules/home/common"];
+      };
+    })
+    // (optionalAttrs guest.homeManager {
+      "${guest.name}" = {
+        imports = ["${self}/modules/home/common"];
+      };
+    });
+}
