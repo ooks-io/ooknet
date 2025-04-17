ea7f37b39e754fdb658c929b58af369017903371
ooks-io <ooks@protonmail.com>
2025-04-17 12:42:00 +1000

    fix: type

--- outputs/lib/builders.nix
+++ outputs/lib/builders.nix
@@ -56,7 +56,7 @@
       darwin
 
       # TODO: this is jank please make better... actually this whole thing is jank
-      (nixos.workstaion + "options.nix")
+      (nixos.workstation + "options.nix")
     ];
 
   isoModules = [
