bfd9b8a4800b6f5361f3b6dd81ee1de7617b7db5
ooks-io <ooks@protonmail.com>
2025-05-19 22:31:00 +1000

    gaming: move udev rules to services.udev

--- modules/nixos/workstation/gaming/emulation.nix
+++ modules/nixos/workstation/gaming/emulation.nix
@@ -8,18 +8,14 @@
   inherit (config.ooknet.workstation) profiles;
 in {
   config = mkIf (elem "gaming" profiles) {
-    services.udev.packages = [
-      pkgs.dolphin-emu
-      (pkgs.writeTextFile {
-        name = "99-nintendo-controller.rules";
-        text = ''
-          SUBSYSTEM=="usb", ENV{DEVTYPE}=="usb_device", \
-          ATTRS{idVendor}=="057e", ATTRS{idProduct}=="0337", \
-          MODE="0666";
-        '';
-        destination = "/etc/udev/rules.d/40-nintendo-controller.rules";
-      })
-    ];
+    services.udev = {
+      packages = [
+        pkgs.dolphin-emu
+      ];
+      extraRules = ''
+        SUBSYSTEM=="usb", ENV{DEVTYPE}=="usb_device", ATTRS{idVendor}=="057e", ATTRS{idProduct}=="0337", MODE="0666"
+      '';
+    };
     boot.extraModulePackages = [
       config.boot.kernelPackages.gcadapter-oc-kmod
     ];
