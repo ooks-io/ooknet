301d7316f03c4dcaed14ce976c9d20f2fc62e855
ooks-io <ooks@protonmail.com>
2025-05-27 16:26:42 +1000

    vesktop: new theme discord_16

--- modules/home/common/communication/vesktop/default.nix
+++ modules/home/common/communication/vesktop/default.nix
@@ -25,139 +25,33 @@ in {
       */
       ''
         /**
-          * @name nix-colors-minimal
-          * @author aoku
-          * @description minimal theme designed with nix colors
+         * @name Discord 1.6 (Gruvbox)
+         * @author dom1torii
+         * @description CS 1.6 + Old Steam Inspired Theme for Discord
+         * @website https://github.com/dom1torii/discord16
+         * @version 0.0.3
         */
 
-        :root {
-          --nix-bg1: #${color.base00};
-          --nix-bg2: #${color.base01};
-          --nix-bg3: #${color.base02};
+        @import url("https://dom1torii.github.io/discord16/src/css/main.css");
 
-          --nix-fg1: #${color.base05};
-          --nix-fg2: #${color.base07};
-          --nix-fg3: #${color.base03};
-          --nix-link: #${color.base0D};
-
-          --nix-accent: #${color.base08};
-          --nix-hi: #${color.base0B};
-
-          --font-mono: ${fonts.monospace.family}, monospace;
-          --font-regular: ${fonts.regular.family}, sans serif;
-
-          /* server collapse */
-          --sb-collapsed-width: 12px;
-          --sb-transition-duration: 0s;
+        * {
+          --font: "${fonts.monospace.family}"; /* Change to "" for default font */
+          --rounded-avatars: false;
+          --steam-logo: false;
         }
 
-        .theme-dark {
-          --background-primary: var(--nix-bg1);
-          --background-secondary: var(--nix-bg1);
-          --background-secondary-alt: var(--nix-bg1);
-          --background-accent: var(--nix-accent);
-          --background-tertiary: var(--nix-bg1);
-          --background-floating: var(--nix-bg1);
-          --background-mentioned: var(--nix-bg1);
-          --background-mentioned-hover: var(--nix-bg1);
-          --background-mobile: var(--nix-bg1);
-          --background-mobile-secondary: var(--nix-bg2);
-          --background-modifier-selected: var(--nix-bg1);
-          --channeltextarea-background:var(--nix-bg1);
-          --background-modifier-hover:var(--nix-bg1);
-          --activity-card-background: var(--nix-bg2);
+        * {
+          --custom-bg: #${color.layout.menu};
+          --custom-secondary-bg: #${color.layout.body};
 
-          --header-primary: var(--nix-fg2);
-          --header-secondary: var(--nix-fg1);
+          --custom-accent: #${color.primary.base};
+          --custom-secondary-accent: #${color.primary.soft4};
 
-          --text-normal: var(--nix-fg1);
-          --text-muted: var(--nix-fg1);
-          --text-link: var(--nix-link);
-          --text-warning: var(--nix-accent);
-          --font-primary: var(--font-mono);
-          --font-headline: var(--font-mono);
-          --font-display: var(--font-mono);
+          --dark-border: #${color.border.inactive};
+          --light-border: #${color.border.active};
 
-          --interactive-normal: var(--nix-fg1); /*base05*/
-          --interactive-hover: var(--nix-hi); /*base0B*/
-          --interactive-active: var(--nix-fg2);
-          --interactive-muted: var(--nix-fg3); /*base03*/
-          --channels-default: var(--nix-fg1);
-
-          --scrollbar-thin-thumb: transparent;
-          --scrollbar-thin-track: transparent;
-          --scrollbar-auto-thumb: var(--nix-fg1);
-          --scrollbar-auto-track:var(--nix-bg1);
-          --scrollbar-auto-scrollbar-color-thumb: var(--nix-accent);
-        }
-
-        .messagesWrapper_ea2b0b {
-            font-family: var(--font-regular);
-        }
-
-        .titleWrapper__482dc {
-          font-family: var(--font-mono);
-        }
-
-        .link__95dc0 /* text channel*/{
-          border-radius: 0px;
-          margin-left: -10px;
-          font-family: var(--font-mono);
-        }
-
-        .container_ca50b9 .avatar_f8541f { /*avatar*/
-          display: none;
-        }
-
-        .form__13a2c /* text input box resize */ {
-          height: 50px;
-          font-family: var(--font-regular);
-        }
-
-        .containerDefault__3187b .wrapper__7bcde:before /* text channel */{
-          content: "";
-          display:inline-block;
-          background: var(--nix-hi);
-          height: 100%;
-          position: absolute;
-          left: 0;
-        }
-
-
-        /* server collapse */
-        .guilds__2b93a /* servers */{
-            overflow: hidden !important;
-            width: var(--sb-collapsed-width, 75px);
-            transition: width var(--sb-transition-duration);
-        }
-        .guilds__2b93a:hover /* expand server bar on hover */{
-            width: 70px;
-            overflow: visible !important;
-            animation: server-bar-overflow 0s linear 0ms forwards
-        }
-        .guilds__2b93a ~ .base__3e6af /* friends list, chat */{
-            position: absolute;
-            left: var(--sb-collapsed-left, var(--sb-collapsed-width));
-            top: var(--sb-collapsed-top, 0px);
-            bottom: var(--sb-collapsed-bottom, 0px);
-            right: var(--sb-collapsed-right, 0px);
-            transition-property: var(--sb-transition-property, left);
-            transition-duration: var(--sb-transition-duration);
-        }
-        .guilds__2b93a:hover ~ .base__3e6af /* friends list, chat */{
-            position: absolute;
-            left: var(--sb-left, 70px);
-            top: var(--sb-top, 0px);
-            bottom: var(--sb-bottom, 0px);
-            right: var(--sb-right, 0px);
-        }
-        @keyframes server-bar-overflow{
-            from{
-                overflow: hidden;
-            }
-            to{
-                overflow: visible;
-            }
+          --custom-text: #${color.typography.text};
+          --custom-secondary-text: #${color.typography.text-bright};
         }
       '';
 
