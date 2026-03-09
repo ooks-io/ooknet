abbd259fccfeaa7dace6aabb438702d114762dd9
ooks-io <ooks@protonmail.com>
2026-03-09 21:32:38 +1100

    ooksdesk: single monitor setup

--- hosts/ooksdesk/hardware.nix
+++ hosts/ooksdesk/hardware.nix
@@ -11,21 +11,11 @@
     features = ["printing" "ssd" "audio" "video"];
     monitors = [
       {
-        name = "DP-1";
-        primary = true;
-        width = 2560;
-        height = 1440;
-        refreshRate = 144;
-        x = 1920;
-        y = 100;
-      }
-      {
         name = "DP-2";
+        primary = true;
         width = 1920;
         height = 1080;
         refreshRate = 180;
-        x = 840;
-        transform = 1;
       }
     ];
   };
