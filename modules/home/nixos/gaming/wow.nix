906925d6fcad1bb0eba20bc02cbd077314a72aa3
ooks-io <ooks@protonmail.com>
2026-03-29 11:26:29 +1100

    wow: add curseforge pkg

--- modules/home/nixos/gaming/wow.nix
+++ modules/home/nixos/gaming/wow.nix
@@ -5,9 +5,16 @@
   ...
 }: let
   inherit (lib) mkIf elem;
+  inherit (builtins) attrValues;
   inherit (osConfig.ooknet.workstation) profiles;
 in {
   config = mkIf (elem "gaming" profiles) {
-    home.packages = [self'.packages.wowup];
+    home.packages = attrValues {
+      inherit
+        (self'.packages)
+        wowup
+        curseforge
+        ;
+    };
   };
 }
