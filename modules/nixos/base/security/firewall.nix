22e35f8ef22638229a778ec80a394e2988399e91
ooks-io <ooks@protonmail.com>
2026-07-11 22:55:53 +1000

    firewall: switch to nftables backend

--- modules/nixos/base/security/firewall.nix
+++ modules/nixos/base/security/firewall.nix
@@ -1,14 +1,20 @@
 {
-  networking.firewall = {
-    enable = true;
+  networking = {
+    # nftables backend: cleaner source-scoped rules (extraInputRules) and
+    # unifies with the nftables fail2ban action already in use
+    nftables.enable = true;
 
-    allowedTCPPorts = [
-      22 # SSH
-      80
-      443
-    ];
+    firewall = {
+      enable = true;
 
-    # dont respond to icmpv4 pings.
-    allowPing = false;
+      allowedTCPPorts = [
+        22 # SSH
+        80
+        443
+      ];
+
+      # dont respond to icmpv4 pings.
+      allowPing = false;
+    };
   };
 }
