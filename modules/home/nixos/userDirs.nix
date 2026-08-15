8b6a0b69b6a0bd03ea336a618538cf802f1b4d45
ooks-io <ooks@protonmail.com>
2026-08-16 00:38:02 +1000

    home: setSessionVariables

--- modules/home/nixos/userDirs.nix
+++ modules/home/nixos/userDirs.nix
@@ -11,6 +11,8 @@
     in {
       enable = true;
       createDirectories = true;
+      # keep exporting XDG_*_DIR session vars, fish functions + hyprcapture rely on them
+      setSessionVariables = true;
       desktop = "${config.home.homeDirectory}/Desktop";
       documents = "${summit}/Documents";
       music = "${summit}/Media/Music";
