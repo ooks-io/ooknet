ccffd0310f00a54b60999e9719ab823324051445
ooks-io <ooks@protonmail.com>
2026-08-16 12:45:12 +1000

    jankyborders: thin square borders

--- modules/darwin/environment/components/jankyborders.nix
+++ modules/darwin/environment/components/jankyborders.nix
@@ -2,22 +2,33 @@
   lib,
   ook,
   config,
+  pkgs,
   ...
 }: let
   inherit (ook) color;
   inherit (lib) mkIf;
   inherit (config.ooknet.workstation) environment;
+
+  jankyborders-thin = pkgs.jankyborders.overrideAttrs (old: {
+    postPatch =
+      (old.postPatch or "")
+      + ''
+        substituteInPlace src/border.h \
+          --replace-fail "#define BORDER_TSMW 8.f" "#define BORDER_TSMW 2.f" \
+          --replace-fail "#define BORDER_TSMN 3.27f" "#define BORDER_TSMN 1.0f"
+      '';
+  });
 in {
   config = mkIf (environment == "aerospace") {
     # jankyborders is a service that adds borders to frames on macos
     services.jankyborders = {
       enable = true;
+      package = jankyborders-thin;
       active_color = "0xff${color.border.active}";
       inactive_color = "0xff${color.border.inactive}";
-      width = 5.0;
-      # what layer the border will sit on, available options:
-      # above, below
-      # warning, this will break things.
+      width = 2.0;
+      hidpi = true;
+      style = "square";
       order = "above";
       # we whitelist applications as certain apps do not play well with borders, namely AppStore
       whitelist = [
