3acdef6e8def2462c5bd19f649e72290fb3e6f1d
ooks-io <ooks@protonmail.com>
2026-09-26 15:18:37 +1000

    nix: drop ooknet cachix substituter, ooksmedia replaces it

--- modules/common/base/nix.nix
+++ modules/common/base/nix.nix
@@ -69,7 +69,6 @@ in {
         "https://cache.nixos.org"
         "https://nix-community.cachix.org"
         "https://neovim-flake.cachix.org"
-        "https://ooknet.cachix.org"
         # harmonia on ooksmedia, tailnet
         "http://ooksmedia.taila3ca6.ts.net:5000"
       ];
@@ -77,7 +76,6 @@ in {
         "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
         "neovim-flake.cachix.org-1:iyQ6lHFhnB5UkVpxhQqLJbneWBTzM8LBYOFPLNH4qZw="
-        "ooknet.cachix.org-1:mtr4ue+8ux58b8mgTGRAG/txxHBnZvgX7Gi3amno+zs="
         "ooksmedia-1:a6jc9+fzOTjzOIqdWXKnbhNB6unU8Z29eUWCg1m0iOY="
       ];
     };
